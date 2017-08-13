83a4bedbba4ec5ec31b37207f023ccea3e04b9c7
tinesone <tlr3@live.nl>
2017-08-13 15:42:43 +0200

    Added Death objective

--- src/main/java/com/cuhka/hvo/HvOPlugin.java
+++ src/main/java/com/cuhka/hvo/HvOPlugin.java
@@ -1,19 +1,33 @@
 package com.cuhka.hvo;
 
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.entity.PlayerDeathEvent;
 import org.bukkit.plugin.java.JavaPlugin;
+import org.bukkit.scoreboard.DisplaySlot;
+import org.bukkit.scoreboard.Objective;
+import org.bukkit.scoreboard.Scoreboard;
 
-public class HvOPlugin extends JavaPlugin {
-
+public class HvOPlugin extends JavaPlugin implements Listener {
 	@Override
-	public void onDisable() {
-		// TODO Auto-generated method stub
-		super.onDisable();
-	}
-
-	@Override
-	public void onLoad() {
-		System.out.println("Hello World");
+	public void onEnable() {
+		System.out.println("Enabled HvO2");
+		getServer().getPluginManager().registerEvents(this, this);
+		Scoreboard board = getServer().getScoreboardManager().getMainScoreboard();
+		Objective dieCount = board.registerNewObjective("dieCount", "deathCount");
+		dieCount.setDisplaySlot(DisplaySlot.BELOW_NAME);
+		dieCount.setDisplayName("Deaths");
 		super.onEnable();
 	}
+	
+	@EventHandler
+	public void defeated(PlayerDeathEvent e) {
+		Player victim = e.getEntity();	
+		Player killer = victim.getKiller();
+		e.setDeathMessage(String.format("%s has been slayed by %s", victim.getName(), killer.getName()));
+	}
+	
 
+	
 }
