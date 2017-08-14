92a10aa5a49ccacbc1d3ac59004f7425ac8de02e
tinesone <tlr3@live.nl>
2017-08-14 17:19:55 +0200

    Added Teams.
    Team Orcs and Team Humans with all their settings needed

--- src/main/java/com/cuhka/hvo/HvOPlugin.java
+++ src/main/java/com/cuhka/hvo/HvOPlugin.java
@@ -1,5 +1,6 @@
 package com.cuhka.hvo;
 
+import org.bukkit.ChatColor;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
@@ -8,24 +9,43 @@ import org.bukkit.plugin.java.JavaPlugin;
 import org.bukkit.scoreboard.DisplaySlot;
 import org.bukkit.scoreboard.Objective;
 import org.bukkit.scoreboard.Scoreboard;
+import org.bukkit.scoreboard.Team;
 
 public class HvOPlugin extends JavaPlugin implements Listener {
+	private static final String AQUA = "\u00a74";
+	private static final String DARK_RED = "\u00a7b";
+
 	@Override
 	public void onEnable() {
-		System.out.println("Enabled HvO2");
+		System.out.println("Enabled HvO");
 		getServer().getPluginManager().registerEvents(this, this);
 		Scoreboard board = getServer().getScoreboardManager().getMainScoreboard();
-		Objective dieCount = board.registerNewObjective("dieCount", "deathCount");
-		dieCount.setDisplaySlot(DisplaySlot.BELOW_NAME);
-		dieCount.setDisplayName("Deaths");
+		createTeam(board, "Orcs", ChatColor.DARK_RED, DARK_RED);
+		createTeam(board, "Humans", ChatColor.AQUA, AQUA);
+	
+		Objective killCount = board.registerNewObjective("killCount", "playerKillCount");
+		killCount.setDisplaySlot(DisplaySlot.BELOW_NAME);
+		killCount.setDisplayName("Kills");
 		super.onEnable();
+	}
+
+	private Team createTeam(Scoreboard board, String name, ChatColor color, String prefix) {
+		Team team = board.getTeam(name);
+		if (team == null){
+			team = board.registerNewTeam(name);
+		}
+		team.setAllowFriendlyFire(false);
+		team.setColor(color);
+		team.setPrefix(prefix);
+		team.setOption(Team.Option.NAME_TAG_VISIBILITY, Team.OptionStatus.ALWAYS);
+		team.setOption(Team.Option.COLLISION_RULE, Team.OptionStatus.FOR_OWN_TEAM);
+		return team;
 	}
 	
 	@EventHandler
 	public void defeated(PlayerDeathEvent e) {
-		Player victim = e.getEntity();	
-		Player killer = victim.getKiller();
-		e.setDeathMessage(String.format("%s has been slayed by %s", victim.getName(), killer.getName()));
+		Player victim = e.getEntity();
+		Player killer = victim.getKiller();		
 	}
 	
 
