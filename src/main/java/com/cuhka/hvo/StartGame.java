c8ba9f9b16f3b063a986bde5b0e1ea9a1ff52fee
tinesone <tlr3@live.nl>
2017-08-29 16:13:34 +0200

    removed unused import

--- src/main/java/com/cuhka/hvo/StartGame.java
+++ src/main/java/com/cuhka/hvo/StartGame.java
@@ -19,7 +19,6 @@ import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.configuration.Configuration;
-import org.bukkit.entity.Entity;
 import org.bukkit.entity.Player;
 import org.bukkit.entity.Zombie;
 import org.bukkit.inventory.ItemStack;
